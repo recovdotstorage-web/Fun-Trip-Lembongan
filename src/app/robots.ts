66d58db51e3b57697f615f10c14b62021538b88a
Deva <threpasty@gmail.com>
2026-05-11 22:18:30 +0700

    update seo friendly

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/admin/',
     },
-    sitemap: 'https://funtriplembongan.com/sitemap.xml',
+    sitemap: `${process.env.NEXT_PUBLIC_APP_URL || 'https://funtriplembongan.com'}/sitemap.xml`,
   };
 }
